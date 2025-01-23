c4115abc3424c8def477e515fc392ee9597f28d4
NTAGOZERA Kevin <ntagozeraKevin@gmail.com>
2025-01-23 16:00:00 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,7 @@
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+
+
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits);
---
+++ function.js
@@ -0,0 +1,16 @@
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(5, 3)); // Output: 15
+
+
+
+
+
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet('Alice')); // Output: Hello, Alice!
